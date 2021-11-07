393a9f808d94c454bb7100d6ff56fb38dae2a9bb
Sandro M dos Santos <sandromarista@gmail.com>
2021-11-06 21:52:31 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -24,9 +24,9 @@ const DonutChart = () => {
     },[]);
 
    
-    //const mockData = {
-      //  series: [477138, 499928, 444867, 220426, 473088],
-        //labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+   // const mockData = {
+      // series: [477138, 499928, 444867, 220426, 473088],
+       // labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     //}
     
     const options = {
